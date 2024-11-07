8854295100368ff6db0111df7d5ae7ff37fc4ad1
alexandreTest1 <alekotevzadze1988@gmail.com>
2024-11-07 23:17:22 +0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int customerAge = 35;
+
+
+        System.out.println("chemi momkhmareblis asaki aris " +  "  +++___  " + customerAge + " weli");
+
+
+    }
+
+}
---
+++ src/main/java/org/example/TestAlex.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class TestAlex {
+    public static void main(String[] args) {
+
+    }
+}
